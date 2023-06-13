126fe939a467d4b72ce8e42e8871d1f59ebbe1c1
Mateusz Gałecki <m.galecki10@gmail.com>
2023-06-13 21:56:48 +0200

    v0.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +11,75 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // napisz swój program tutaj. Do wczytywania danych użyj przekazanego w parametrze scannera
+        List<Integer> numbers = new ArrayList<>();
+        int number;
+        while ((number = scanner.nextInt()) >= 0) {
+            numbers.add(number);
+        }
+
+        getReverseNumbers(numbers);
+
+        getSumOfAllElementsFromList(numbers);
+
+        int minNumber = getMinNumber(numbers);
+        System.out.println("Najmniejsza liczba na liście to " + minNumber);
+
+        int maxNumber = getMaxNumber(numbers);
+        System.out.println("Nawiększa liczba na liście to " + maxNumber);
+
+    }
+
+    private static void getReverseNumbers(List<Integer> numbers) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Collections.reverse(numbers);
+        for (int i = 0; i < numbers.size(); i++) {
+            stringBuilder.append(numbers.get(i));
+            if (i < numbers.size() - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+
+        String reverseResult = stringBuilder.toString();
+        System.out.println(reverseResult);
+    }
+
+    private static void getSumOfAllElementsFromList(List<Integer> numbers) {
+        Collections.reverse(numbers);
+        StringBuilder resultSum = new StringBuilder();
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            Integer numberText = numbers.get(i);
+            resultSum.append(numberText);
+            sum += numbers.get(i);
+            if (i == numbers.size() - 1) {
+                resultSum.append(" = ");
+                resultSum.append(sum);
+            } else {
+                resultSum.append(" + ");
+            }
+        }
+        String sumResult = resultSum.toString();
+        System.out.println(sumResult);
+    }
+
+    private static int getMinNumber(List<Integer> numbers) {
+        int nextNumber = numbers.get(0);
+        for (Integer number : numbers) {
+            if (nextNumber > number) {
+                nextNumber = number;
+            }
+        }
+        return nextNumber;
+
+    }
+
+    private static int getMaxNumber(List<Integer> numbers) {
+        int nextNumber = numbers.get(0);
+        for (Integer number : numbers) {
+            if (nextNumber < number) {
+                nextNumber = number;
+            }
+        }
+        return nextNumber;
     }
 }
